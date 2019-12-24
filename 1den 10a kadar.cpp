fcf7faa5d64eed048739bcb629576eea24f3b0e3
Elçin Çelik <elcincelik@users.noreply.github.com>
2019-12-24 15:47:27 +0300

    1 ile 10 arasındaki sayıların carpım tablosu

---
+++ 1den 10a kadar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+int j;
+	for ( i=5;i<=10;i++)
+	{
+		for
+	(j=1;j<=10;j++)
+	{
+		cout<<j<<"*"<<i<<"="<<j*i<<"\t";
+	}
+	cout<<endl;
+	}
+}
